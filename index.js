6a458943d5f1cfd2100d83aa2e7e8e8bcc16a846
baran0607 <153203422+baran0607@users.noreply.github.com>
2025-03-03 17:22:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,35 +3,9 @@ import ReactDOM from 'react-dom/client';
 import App from './taskmanagement/App';
 
 
-
-// const store = configureStore({
-//   reducer: todoReducer,
-// });
-
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  //this is for todolist
-  // <Provider store={store}>
-  //   <App />
-  // </Provider>
-  
-  //this is for redux increment and drecrement
-  // <App />
-
-  //this is for displayuser
-  // <Provider store={store}>
-  //   <App />
-  // </Provider>
-
-  //this is for taskmanagement
   <App />
-
-  //this is for weather app
-  // <Provider store={store}>
-  //   <App />
-  // </Provider>,
- 
 );
 
 
